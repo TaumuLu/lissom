647812d46901b348d2b3934012ba06b9d760c684
TaumuLu <972409545@qq.com>
2019-01-03 23:26:55 +0800

    rm dist folder

--- dist/lib/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ASSETS_MANIFEST = 'assets-manifest.json';
-exports.CONFIG_MANIFEST = 'config-manifest.json';
-exports.RUNTIME_NAME = 'webpack-runtime';
-exports.HTML_WEBPACK_PLUGIN = 'HtmlWebpackPlugin';
-exports.GLOBAl_VARIABLE = '__global_variable__';
-exports.JSONP_FUNCTION = 'webpackJsonp';
-//# sourceMappingURL=constants.js.map
