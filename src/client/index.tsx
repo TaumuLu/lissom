76299bd9b6d55caf721a5b1c2e05090ffa6e42bb
TaumuLu <972409545@qq.com>
2019-02-15 19:21:19 +0800

    feat: compatible webpack normal chunk

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,62 +1,67 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { pathMap } from '../lib/async'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { pathMap } from '../lib/async';
 
 declare global {
   interface Window {
     __SSR_DATA__: {
-      props: any
-      pathname: string
-      asyncProps: any[]
-      clientRender: boolean
-    }
-    __SSR_LOADED_PAGES__: any[]
-    __SSR_REGISTER_PAGE__: Function
+      props: any;
+      pathname: string;
+      asyncProps: any[];
+      clientRender: boolean;
+    };
+    __SSR_LOADED_PAGES__: any[];
+    __SSR_REGISTER_PAGE__: Function;
   }
 }
 
 // 客户端挂载dom节点，webpack入口处注入
-if (typeof window !== 'undefined') {
+if (
+  typeof window !== 'undefined' &&
+  typeof window.__SSR_REGISTER_PAGE__ !== 'undefined'
+) {
   // 异步延迟至当前入口模块导出后再执行，入口模块为导出的react组件，一定会是同步执行
   Promise.resolve().then(() => {
-    const { props, pathname, asyncProps, clientRender = true } = window.__SSR_DATA__
-    if (!clientRender) return void 0
+    const {
+      props,
+      pathname,
+      asyncProps,
+      clientRender = true,
+    } = window.__SSR_DATA__;
+    if (!clientRender) return void 0;
 
-    let isInitialRender = true
+    let isInitialRender = true;
     function renderReactElement(reactEl, domEl) {
       // The check for `.hydrate` is there to support React alternatives like preact
       if (isInitialRender && typeof ReactDOM.hydrate === 'function') {
-        ReactDOM.hydrate(reactEl, domEl)
-        isInitialRender = false
+        ReactDOM.hydrate(reactEl, domEl);
+        isInitialRender = false;
       } else {
-        ReactDOM.render(reactEl, domEl)
+        ReactDOM.render(reactEl, domEl);
       }
     }
 
-    const initialRoute = window.__SSR_LOADED_PAGES__.shift()
-    const mathValue = pathMap.get(pathname)
+    const initialRoute = window.__SSR_LOADED_PAGES__.shift();
+    const mathValue = pathMap.get(pathname);
     // 设置回服务端获取的异步值
     if (mathValue) {
-      mathValue.setValue(asyncProps)
+      mathValue.setValue(asyncProps);
     }
 
-    const routers = {}
+    const routers = {};
     const registerPage = (route, { page: Component }) => {
-      routers[route] = Component
+      routers[route] = Component;
 
       if (isInitialRender && route === initialRoute) {
-        const appContainer = document.getElementById('__ssr__')
-        renderReactElement(
-          <Component {...props} />,
-          appContainer
-        )
+        const appContainer = document.getElementById('__ssr__');
+        renderReactElement(<Component {...props} />, appContainer);
       }
-    }
+    };
 
     window.__SSR_LOADED_PAGES__.forEach(([r, f]) => {
-      registerPage(r, f)
-    })
-    delete window.__SSR_LOADED_PAGES__
-    window.__SSR_REGISTER_PAGE__ = registerPage
-  })
+      registerPage(r, f);
+    });
+    delete window.__SSR_LOADED_PAGES__;
+    window.__SSR_REGISTER_PAGE__ = registerPage;
+  });
 }
--- src/webpack/plugins/pages-plugin.ts
+++ src/webpack/plugins/pages-plugin.ts
@@ -1,31 +1,35 @@
-import { ConcatSource } from 'webpack-sources'
+import { ConcatSource } from 'webpack-sources';
 
 export default class PagesPlugin {
   public apply(compiler) {
-    compiler.hooks.compilation.tap('PagesPlugin', (compilation) => {
-      compilation.moduleTemplates.javascript.hooks.render.tap('PagesPluginRenderRegister', (moduleSourcePostModule, module, options) => {
-        const { chunk } = options
+    compiler.hooks.compilation.tap('PagesPlugin', compilation => {
+      compilation.moduleTemplates.javascript.hooks.render.tap(
+        'PagesPluginRenderRegister',
+        (moduleSourcePostModule, module, options) => {
+          const { chunk } = options;
 
-        // 排除非入口模块
-        if (chunk.entryModule !== module) {
-          return moduleSourcePostModule
+          // 排除非入口模块
+          if (chunk.entryModule !== module) {
+            return moduleSourcePostModule;
+          }
+
+          // 排除非项目入口模块
+          if (compilation.compiler !== compiler) {
+            return moduleSourcePostModule;
+          }
+
+          // 包装入口模块注册函数，供客户端查找调用
+          const source = new ConcatSource(
+            'var __SSR_REGISTER_PAGE__ = __SSR_REGISTER_PAGE__ || function(r, f) { return f() }\n',
+            `__SSR_REGISTER_PAGE__('${chunk.name}', function() {\n`,
+            moduleSourcePostModule,
+            '\nreturn { page: module.exports.default }',
+            '});'
+          );
+
+          return source;
         }
-
-        // 排除非项目入口模块
-        if (compilation.compiler !== compiler) {
-          return moduleSourcePostModule
-        }
-
-        // 包装入口模块注册函数，供客户端查找调用
-        const source = new ConcatSource(
-          `__SSR_REGISTER_PAGE__('${chunk.name}', function() {\n`,
-          moduleSourcePostModule,
-          '\nreturn { page: module.exports.default }',
-          '});'
-        )
-
-        return source
-      })
-    })
+      );
+    });
   }
 }
